4817e99c6135a42f44853d2cd9882e51019bb1fb
LucaGervasi07 <95702880+LucaGervasi07@users.noreply.github.com>
2022-02-16 17:54:39 +0100

    Ablou

--- app/components/sketches/random_circle.js
+++ app/components/sketches/random_circle.js
@@ -10,6 +10,7 @@ const randomcircle = {
   name: "Random Circle",
   sketch: (sketch) => {
     let mouseIsInCanvas = false;
+    let strokeValues, strokeweightValues;
 
     sketch.setup = () => {
       let cnv = sketch.createCanvas(sketchWidth, sketchHeight);
@@ -24,6 +25,9 @@ const randomcircle = {
       document
         .querySelector("#drawing-canvas")
         .addEventListener("mouseout", () => (mouseIsInCanvas = false));
+      setupResetControl();
+      createSizeControl();
+      strokeweightValues = sketch.setupStrokeweightControl();
     };
 
     sketch.draw = () => {
@@ -46,6 +50,40 @@ const randomcircle = {
         drawCircle(sketch.random(0, 400), sketch.random(0, 600), newD);
       }
     }
+
+    sketch.setupStrokeweightControl = () => {
+      let element = document.createElement("input");
+      let control = createControl(element, true, {
+        name: "Stroke Weight",
+        type: "range",
+        max: 20,
+        min: 1,
+        value: 5,
+      });
+      element.addEventListener("input", () => {
+        strokeweightValues = element.value;
+        control.innerHTML = strokeweightValues;
+        sketch.strokeWeight(strokeweightValues);
+      });
+      return element.value;
+    };
+
+    const setupResetControl = () => {
+      let element = document.createElement("a");
+      createControl(element, false, {
+        class: "button",
+      });
+      element.innerHTML = "Reset";
+
+      element.addEventListener("click", () => {
+        resetSketch();
+      });
+      element.addEventListener("input", () => {
+        control.innerHTML = element.value;
+        quantity = element.value;
+        resetSketch();
+      });
+    };
   },
 };
 
